660bf54d8fcf86b9c44af029a95111064bb261f1
ishankashyap <ikashyap@ualberta.ca>
2024-08-28 21:16:14 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { renderWeather } from "./render";
 const submitBtn = document.getElementById("submit-btn");
 
 submitBtn.addEventListener("click", async () => {
+    const location = document.getElementById("location").value;
     const data = await getWeatherData();
     if (data) {
-        renderWeather(data);
+        renderWeather(data, location);
     }
 });
--- src/render.js
+++ src/render.js
@@ -1,5 +1,22 @@
 import "./styles.css";
 
-export const renderWeather = (data) => {
-    console.log(data);
+export const renderWeather = (data, location) => {
+    const content = document.getElementById("content");
+
+    content.innerHTML = '';
+
+    const weatherContent = document.createElement("div");
+
+    weatherContent.innerHTML = `
+    <div>
+        <h2>Weather Report ${location}:</h2>
+        <p>Conditions: ${data.conditions}</p>
+        <p>Temperature: ${data.temperature}°C</p>
+        <p>Feels Like: ${data.feelsLike}°C</p>
+        <p>Humidity: ${data.humidity}%</p>
+    </div>
+    `;
+
+    content.appendChild(weatherContent);
+
 }
